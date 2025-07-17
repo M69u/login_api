8b276bcf4f9a52dfc7cea673f885e53ef56cf68b
M69u <manu.22ai022@sode-edu.in>
2025-07-17 10:02:46 +0530

    Initialising the echo instance

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
-package main 
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Logger.Fatal(e.Start(":8080"))
+}
